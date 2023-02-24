49ff9cb00734c836dd9ddbfcc25b41d6aa4bfbcb
licetrosales <licetrosales@gmail.com>
2023-02-24 19:15:40 +0100

    Add list of cakes

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ let app = express();
 let router = express.Router();
 app.use('/api/', router);
 
+let cakes = [
+    {"id": 1, "name": "Mango"},
+    {"id": 2, "name": "Apple"},
+    {"id": 3, "name": "Cinamon"}
+];
+
 app.get('/', (req, res, next) => {
-    res.send("Apple");
+    res.send(cakes);
 });
 
 var server = app.listen(5000, () => {

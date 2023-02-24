8d5496a2504a4e9821e35baa56f529b66cc4abcd
licetrosales <licetrosales@gmail.com>
2023-02-24 19:47:13 +0100

    Refactor data in a function that returns a hard-coded array

---
+++ repos/cakeRepos.js
@@ -0,0 +1,10 @@
+let cakeRepo = {
+    get: () => {
+        return [
+            {"id": 1, "name": "Mango"},
+            {"id": 2, "name": "Apple"},
+            {"id": 3, "name": "Cinamon"}
+        ];
+    }
+};
+module.exports = pieRepo;

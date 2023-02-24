def0a25c8f267cc0208f26fe4d7832e4c366a66d
licetrosales <licetrosales@gmail.com>
2023-02-24 19:33:51 +0100

    Wrap an envelope around data

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ let app = express();
 let router = express.Router();
 app.use('/api/', router);
 
-let cakes = [
-    {"id": 1, "name": "Mango"},
-    {"id": 2, "name": "Apple"},
-    {"id": 3, "name": "Cinamon"}
-];
-
+let cakes =
+    [
+        {"id": 1, "name": "Mango"},
+        {"id": 2, "name": "Apple"},
+        {"id": 3, "name": "Cinamon"}
+    ];
 app.get('/', (req, res, next) => {
-    res.send(cakes);
+    res.status(200).json({
+        "status": 200,
+        "statusText": "OK",
+        "message": "All cakes retrieved",
+        "data": cakes
+    });
 });
 
 var server = app.listen(5000, () => {

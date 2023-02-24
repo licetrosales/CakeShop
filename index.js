25de4c9c4520e5d4f564a7ada4c799a50b7c34a0
licetrosales <licetrosales@gmail.com>
2023-02-24 18:46:57 +0100

    Add nodemon dependency to package.json

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.use('/api/', router);
 app.get('/', (req, res, next) => {
     res.send("Apple");
 });
+
+var server = app.listen(5000, () => {
+    console.log("Node server is running on http://localhost:5000..");
+});

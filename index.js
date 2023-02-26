a8fb0581a7fb066353e079662eb48496e28b7306
licetrosales <licetrosales@gmail.com>
2023-02-26 12:20:27 +0100

    Add search for id & name functionality

--- index.js
+++ index.js
@@ -20,9 +20,26 @@ router.get('/', (req, res, next) => {
     });
 
 });
+router.get('/search', (req, res, next) => {
+    let searchObject = {
+        "id": req.query.id,
+        "name": req.query.name
+    };
+    cakeRepo.search(searchObject, (data) => {
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "All cakes retrieved",
+            "data": data
+        });
+    }, (err) => {
+        next(err);
+    });
+});
+
 router.get('/:id', (req, res, next) => {
 
-    cakeRepo.getById( req.params.id, (data) => {
+    cakeRepo.getById(req.params.id, (data) => {
         if (data) {
             res.status(200).json({
                 "status": 200,
@@ -34,15 +51,17 @@ router.get('/:id', (req, res, next) => {
             res.status(404).json({
                 "status": 404,
                 "statusText": "Not found",
-                "message": "Cake '" + req.params.id +"' could not be found.",
+                "message": "Cake '" + req.params.id + "' could not be found.",
                 "error": {
                     "code": "NOT FOUND",
-                    "message": "Cake '" + req.params.id +"' could not be found."
+                    "message": "Cake '" + req.params.id + "' could not be found."
                 }
             });
         }
-    }) ;
+    });
 });
+
+
 
 var server = app.listen(5000, () => {
     console.log("Node server is running on http://localhost:5000..");
--- repos/cakeRepo.js
+++ repos/cakeRepo.js
@@ -4,13 +4,30 @@ const FILE_NAME = "./assets/cakes.json";
 
 let cakeRepo = {
     get: (resolve, reject) => {
-       fs.readFile(FILE_NAME, (err, data) => {
-         if(err) {
-             reject(err);
-         }  else {
-             resolve(JSON.parse(data));
-         }
-       });
+        fs.readFile(FILE_NAME, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(data));
+            }
+        });
+    },
+    search: (searchObject, resolve, reject) => {
+        fs.readFile(FILE_NAME, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                let cakes = JSON.parse(data);
+                if (searchObject) {
+                    cakes = cakes.filter(
+                        p => (searchObject.id ? p.id == searchObject.id : true) &&
+                            (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true)
+                    );
+                }
+                resolve(cakes);
+            }
+        });
+
     },
 
     getById: (id, resolve, reject) => {
@@ -22,6 +39,7 @@ let cakeRepo = {
                 resolve(request);
             }
         });
-    }
+    },
+
 };
 module.exports = cakeRepo;

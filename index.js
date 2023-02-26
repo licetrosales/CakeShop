36204e5356dc8d66c21dcde612399a3e053a93f8
licetrosales <licetrosales@gmail.com>
2023-02-26 10:51:05 +0100

    Add get by id request & infos (status-code 200, status-text OK % message)

--- index.js
+++ index.js
@@ -20,6 +20,29 @@ router.get('/', (req, res, next) => {
     });
 
 });
+router.get('/:id', (req, res, next) => {
+
+    cakeRepo.getById( req.params.id, (data) => {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "All cakes retrieved",
+                "data": data
+            });
+        } else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not found",
+                "message": "Cake '" + req.params.id +"' could not be found.",
+                "error": {
+                    "code": "NOT FOUND",
+                    "message": "Cake '" + req.params.id +"' could not be found."
+                }
+            });
+        }
+    }) ;
+});
 
 var server = app.listen(5000, () => {
     console.log("Node server is running on http://localhost:5000..");

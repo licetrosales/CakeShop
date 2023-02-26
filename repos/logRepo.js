ce893f72382316b41d238f61c69e6f72a8ea56d9
licetrosales <licetrosales@gmail.com>
2023-02-26 21:59:28 +0100

    Add function to write error to a log file

---
+++ repos/logRepo.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+
+const FILE_NAME = './logs/log.txt';
+
+let logRepo = {
+    write: (data, resolve, reject) => {
+        let toWrite = "*".repeat(80) + '\r\n';
+        toWrite += "Date/Time: " + new Date().toLocaleDateString()+ '\r\n';
+        toWrite += "Exception Info: " + JSON.stringify(data) + '\r\n';
+        toWrite += "*".repeat(80) + '\r\n';
+    }
+};
